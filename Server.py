cb507c10bb12472ee1b8230e221cc236e16658cf
OlakunleAbiola <olakunleabiola2016@gmail.com>
2023-12-04 18:50:48 -0800

    ping successful

--- Server.py
+++ Server.py
@@ -1,10 +1,39 @@
+# oLakunleabiola
+# CNE 335 Fall
+
+import os
+
+
 class Server:
     """ Server class for representing and manipulating servers. """
 
     def __init__(self, server_ip):
-        # TODO -
         self.server_ip = server_ip
 
     def ping(self):
-        # TODO - Use os module to ping the server
-        return
+        try:
+
+            response = os.system(f"ping -n 4 {self.server_ip}")  # Use os module to ping the server
+            if response == 0:
+                return f"Server {self.server_ip} is reachable."
+            else:
+                return f"Server {self.server_ip} is unreachable."
+        except Exception as e:
+            return f"Error while pinging the server: {str(e)}"
+
+
+def print_program_info():
+    print("Server Automator v0.1 by Olakunleabiola")
+
+
+# This is the entry point to our program
+if __name__ == '__main__':
+    print_program_info()
+
+
+    server_ip = "34.221.172.36"    # Create a Server object
+    server = Server(server_ip)
+
+
+    result = server.ping()  # Call Ping method and print the results
+    print(result)
--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 # This is the template code for the CNE335 Final Project
-# Justin Ellis
+# oLakunleabiola
 # CNE 335 Fall
 
+import subprocess
+
+class Server:
+    def __init__(self, ip_address):
+        self.ip_address = ip_address
+
+    def ping(self):
+        try:
+
+            result = subprocess.run(['ping', '-c', '4', self.ip_address], capture_output=True, text=True, check=True)  # Run the ping command and capture the output
+            return result.stdout
+        except subprocess.CalledProcessError as e:
+            return f"Error: {e}"
+
 def print_program_info():
-    # TODO - Change your name
     print("Server Automator v0.1 by Your Name")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
-    # TODO - Create a Server object
-    # TODO - Call Ping method and print the results
+
+
+    server_ip = '34.221.172.36' # TODO - Replace '34.221.172.36' with the actual IP address you want to ping
+
+
+    server = Server(server_ip)  # Create a Server object
+
+
+    ping_result = server.ping()   # Call Ping method and print the results
+    print(f"Ping Results for {server_ip}:\n{ping_result}")
